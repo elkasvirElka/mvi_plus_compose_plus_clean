229ec7135837ea803e6321c05ddf981a9647a1eb
ella_mi <elvira@heliumfoot.com>
2024-10-20 13:36:44 -0500

    Added reducer to see what it is. But it's not used, I don't like it

--- app/src/main/java/com/example/mvi_plus_compose_plus_clean/presentation/MovieListViewModel.kt
+++ app/src/main/java/com/example/mvi_plus_compose_plus_clean/presentation/MovieListViewModel.kt
@@ -3,11 +3,13 @@ package com.example.mvi_plus_compose_plus_clean.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mvi_plus_compose_plus_clean.data.entity.MovieInfo
+import com.example.mvi_plus_compose_plus_clean.data.entity.MovieResult
 import com.example.mvi_plus_compose_plus_clean.domain.MovieRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class MovieListViewModel(private val repository: MovieRepository) : ViewModel() {
 
@@ -43,6 +45,27 @@ class MovieListViewModel(private val repository: MovieRepository) : ViewModel() 
    private fun loadMovieDetails(movieId: Int) {
         // Load movie details from repository
     }
+
+    // The reducer function which processes the state based on the response and the event
+    //In my opinion this is not very nice code, I prefer to handle states in functions like loadMovies
+    /*private fun reducer(
+        currentState: MovieListState,
+        event: MovieListEvent,
+        response: Response<MovieResult>? = null
+    ): MovieListState {
+        return when (event) {
+            is MovieListEvent.LoadMovies -> {
+                if (response != null && response.isSuccessful) {
+                    response.body()?.results?.let {
+                        MovieListState.Loaded(it)
+                    } ?: MovieListState.Error("No data available")
+                } else {
+                    MovieListState.Error("Error loading movies")
+                }
+            }
+            else -> currentState // Return the current state for other events
+        }
+    }*/
 }
 
 sealed class MovieListEvent {
